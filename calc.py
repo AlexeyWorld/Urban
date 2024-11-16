5557f9fbc258e68a0f52f6b2b1aea5c972b05e0b
AlexeyWorld <contract44fz@gmail.com>
2024-11-16 14:49:31 +0300

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,14 @@
+num1 = input("Hello, What is your First Number?\n")
+operation = input("Operation?\n")
+num2 = input("Your Second Number?\n")
+floatnum1 = float(num1)
+floatnum2 = float(num2)
+if operation == "+":
+ output=floatnum1+floatnum2
+if operation == "-":
+ output=floatnum1-floatnum2
+if operation == "*":
+ output=floatnum1*floatnum2
+if operation == "/":
+ output=floatnum1/floatnum2
+print("Your Answer: "+str(output))
