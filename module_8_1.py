bdfedd5ad01b032783d5488a2a78671abc7544e5
AlexeyWorld <contract44fz@gmail.com>
2024-12-31 10:18:31 +0300

    Update module_8_1.py

--- module_8_1.py
+++ module_8_1.py
@@ -2,7 +2,7 @@ def add_everything_up(a, b):
     try:
 
         if isinstance(a, (int, float)) and isinstance(b, (int, float)):
-            return a + b
+            return ("{:.6}".format(a + b))
 
         elif isinstance(a, str) and isinstance(b, str):
             return a + b
